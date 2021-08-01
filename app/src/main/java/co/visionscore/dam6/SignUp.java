4692880f93d7a8cc449b129f4a96afca005b51b7
JulianLQ <jlopezq1@eafit.edu.co>
2021-07-31 19:14:40 -0500

    FIrebase Register User

--- app/src/main/java/co/visionscore/dam6/SignUp.java
+++ app/src/main/java/co/visionscore/dam6/SignUp.java
@@ -1,14 +1,106 @@
 package co.visionscore.dam6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
+
+    private EditText newEmail;
+    private EditText newPassword;
+    private EditText rePassword;
+    private Button signUp;
+
+    FirebaseAuth mAuth;
+    private static final String TAG = "EmailPassword";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        newEmail = findViewById(R.id.et_register_username);
+        newPassword = findViewById(R.id.et_register_password);
+        rePassword = findViewById(R.id.et_repeat_password);
+        signUp = findViewById(R.id.btn_register_signup);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = newEmail.getText().toString();
+                String password = newPassword.getText().toString();
+                String rePass = rePassword.getText().toString();
+
+                if (!email.isEmpty() && !password.isEmpty() && !rePass.isEmpty()){
+                    if (password.equals(rePass)){
+                        if(password.length()>5){
+                            createAccount(email, password);
+                        }else{
+                            Toast.makeText(SignUp.this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }else{
+                    Toast.makeText(SignUp.this, "Some fields are empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            reload();
+        }
+    }
+
+    private void createAccount(String email, String password) {
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUp.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+
+    private void reload() { }
+
+    private void updateUI(FirebaseUser user) {
+
     }
 }
+
+
